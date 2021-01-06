11c2cf61da1849485cd2b3fc9f0ed120b90bc367
carina1754 <bulebed1754@gmail.com>
2021-01-06 23:55:57 +0900

    2021-01-06-23:55

--- v1.py
+++ v1.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 import glob
 import random
+import math
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -28,7 +29,7 @@ def preprocess_data(data, is_train=True):
     
     temp = data.copy()
     temp = temp[['Hour', 'TARGET', 'DHI', 'DNI', 'WS', 'RH', 'T']]
-
+    temp = data.assign(GHI=lambda x:x['DHI']+x['DNI']*np.tan((180*(x['Hour']+1)/24)-90))
     if is_train==True:          
     
         temp['Target1'] = temp['TARGET'].shift(-48).fillna(method='ffill')
@@ -39,8 +40,6 @@ def preprocess_data(data, is_train=True):
 
     elif is_train==False:
         
-        temp = temp[['Hour', 'TARGET', 'DHI', 'DNI', 'WS', 'RH', 'T']]
-                              
         return temp.iloc[-48:, :]
 
 
@@ -58,3 +57,51 @@ for i in range(81):
 
 X_test = pd.concat(df_test)
 
+print(X_test[:48])
+
+
+from sklearn.model_selection import train_test_split
+X_train_1, X_valid_1, Y_train_1, Y_valid_1 = train_test_split(df_train.iloc[:, :-2], df_train.iloc[:, -2], test_size=0.3, random_state=0)
+X_train_2, X_valid_2, Y_train_2, Y_valid_2 = train_test_split(df_train.iloc[:, :-2], df_train.iloc[:, -1], test_size=0.3, random_state=0)
+
+quantiles = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+
+from lightgbm import LGBMRegressor
+
+# Get the model and the predictions in (a) - (b)
+def LGBM(q, X_train, Y_train, X_valid, Y_valid, X_test):
+    
+    # (a) Modeling  
+    model = LGBMRegressor(objective='quantile', alpha=q,
+                         n_estimators=10000, bagging_fraction=0.7, learning_rate=0.027, subsample=0.7)                   
+                         
+                         
+    model.fit(X_train, Y_train, eval_metric = ['quantile'], 
+          eval_set=[(X_valid, Y_valid)], early_stopping_rounds=600,verbose=1000)
+
+    # (b) Predictions
+    pred = pd.Series(model.predict(X_test).round(2))
+    return pred, model
+
+def train_data(X_train, Y_train, X_valid, Y_valid, X_test):
+    
+    LGBM_models=[]
+    LGBM_actual_pred = pd.DataFrame()
+
+    for q in quantiles:
+        print(q)
+        pred , model = LGBM(q, X_train, Y_train, X_valid, Y_valid, X_test)
+        LGBM_models.append(model)
+        LGBM_actual_pred = pd.concat([LGBM_actual_pred,pred],axis=1)
+
+    LGBM_actual_pred.columns=quantiles
+    
+    return LGBM_models, LGBM_actual_pred
+
+models_1, results_1 = train_data(X_train_1, Y_train_1, X_valid_1, Y_valid_1, X_test)
+models_2, results_2 = train_data(X_train_2, Y_train_2, X_valid_2, Y_valid_2, X_test)
+
+submission.loc[submission.id.str.contains("Day7"), "q_0.1":] = results_1.sort_index().values
+submission.loc[submission.id.str.contains("Day8"), "q_0.1":] = results_2.sort_index().values
+
+submission.to_csv('/Users/jungsuuann/Downloads/data/submission_v1.csv', index=False)
