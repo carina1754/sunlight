0aab5edfe01b08d534481e61e77df0840077aa06
carina1754 <bulebed1754@gmail.com>
2021-01-08 22:27:47 +0900

    2021-01-08-22:27

--- v2.py
+++ v2.py
@@ -1,30 +1,110 @@
-
-import pandas as pd
 import numpy as np
+#import tensorflow as tf
 import matplotlib.pyplot as plt
-from keras.models import Sequential
-from keras.layers import Dense, Activation
-from tensorflow.keras.backend import mean, maximum
-from keras.models import Sequential
-from keras.layers import Dense, Activation
-model = Sequential([
-    Dense(32, input_shape=(784,)),
-    Activation('relu'),
-    Dense(10),
-    Activation('softmax'),
-])
-def quantile_loss(q, y, pred):
-  err = (y-pred)
-  return mean(maximum(q*err, (q-1)*err), axis=-1)
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+from sklearn.preprocessing import MinMaxScaler
 
-x = pd.read_csv('/Users/jungsuuann/Downloads/data/train/train.csv')
-y = pd.read_csv('/Users/jungsuuann/Downloads/data/submission.csv')
 
-q_lst = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+x1_data = []#time
+x2_data = []
+x3_data = []
+x4_data = []
+x5_data = []
+x6_data = []#tempertaure
+y_data = []#target
 
-for q in q_lst:
-  model = Sequential()
-  model.add(Dense(10))
-  model.add(Dense(1))   
-  model.compile(loss=lambda y,pred: quantile_loss(q,y,pred), optimizer='adam')
-  model.fit(x,y, epoch=300)
+
+input_file = './data/train/train.csv'
+
+with open(input_file) as file:
+  file.readline()
+  for line in file:
+    temp1 = 0
+    temp1_1 = float(line.split(',')[1])#hour
+    temp1_2 = float(line.split(',')[2])#minute
+    temp2 = float(line.split(',')[3])#
+    temp3 = float(line.split(',')[4])#
+    temp4 = float(line.split(',')[5])#
+    temp5 = float(line.split(',')[6])#
+    temp6 = float(line.split(',')[7])#temperature
+    tempY = float(line.split(',')[8])#target
+
+    if tempY == 0: continue
+
+    if temp1_2 == 0: 
+      temp1 = temp1_1
+    else : temp1 = temp1_1 + 0.5
+
+    x1_data.append(temp1)
+    x2_data.append(temp2)
+    x3_data.append(temp3)
+    x4_data.append(temp4)
+    x5_data.append(temp5)
+    x6_data.append(temp6)
+    y_data.append(tempY)
+
+
+
+def min_max_scaler(mylist):
+  max = mylist[0]
+  min = mylist[0]
+  newlist = []
+  for i in range(len(mylist)):
+    if max < mylist[i]: max = mylist[i]
+    if min > mylist[i]: min = mylist[i]
+
+
+  for j in range(len(mylist)):
+    newelement = (mylist[j] - min) / (max - min) 
+    newlist.append(newelement)
+
+  return newlist
+
+
+x1_data = min_max_scaler(x1_data)
+x2_data = min_max_scaler(x2_data)
+x3_data = min_max_scaler(x3_data)
+x4_data = min_max_scaler(x4_data)
+x5_data = min_max_scaler(x5_data)
+x6_data = min_max_scaler(x6_data)
+
+# 변수
+x1 = tf.placeholder(tf.float32)
+x2 = tf.placeholder(tf.float32)
+x3 = tf.placeholder(tf.float32)
+x4 = tf.placeholder(tf.float32)
+x5 = tf.placeholder(tf.float32)
+x6 = tf.placeholder(tf.float32)
+Y = tf.placeholder(tf.float32)
+
+w1 = tf.Variable(tf.random_normal([1]), name='weight1')
+w2 = tf.Variable(tf.random_normal([1]), name='weight2')
+w3 = tf.Variable(tf.random_normal([1]), name='weight3')
+w4 = tf.Variable(tf.random_normal([1]), name='weight4')
+w5 = tf.Variable(tf.random_normal([1]), name='weight5')
+w6 = tf.Variable(tf.random_normal([1]), name='weight6')
+b = tf.Variable(tf.random_normal([1]), name='bias')
+ 
+# 가설
+hypothesis = x1*w1 + x2*w2 + x3*w3 + x4*w4 + x5*w5 + x6*w6 + b
+k=0.01
+# cost/loss function
+cost = tf.reduce_mean(tf.square(hypothesis - Y))
+# cost function 최소화
+#optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-5)
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=k)
+train = optimizer.minimize(cost)
+
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+for i in range(100):
+      k=i/1000
+      if i>0:
+        for step in range(1001):
+          cost_val, hy_val,_  = sess.run([cost, hypothesis, train], feed_dict={x1: x1_data, x2: x2_data, x3: x3_data, x4: x4_data, x5: x5_data, x6: x6_data, Y: y_data})
+          if step == 1000:
+            print(i," runing  ", step, "\nCost: ", cost_val, "\nPrediction:\n", hy_val)
+
+print(sess.run(w1),sess.run(w2),sess.run(w3),sess.run(w4),sess.run(w5),sess.run(w6),sess.run(b))
--- v5.py
+++ v5.py
@@ -1,84 +1,96 @@
-import tensorflow as tf
-import tensorflow.compat.v1 as tf
-tf.disable_v2_behavior()
-# 데이터
-import pandas as pd
 import numpy as np
-import os
-import glob
-import random
-import math
+#import tensorflow as tf
 import matplotlib.pyplot as plt
-import warnings
-warnings.filterwarnings("ignore")
-train = pd.read_csv('/Users/jungsuuann/Downloads/data/train/train.csv')
-submission = pd.read_csv('/Users/jungsuuann/Downloads/data/sample_submission.csv')
-print(np.tan(0.5))
-def preprocess_data(data, is_train=True):
-    temp = data.copy()
-    temp = temp[['Hour','Minute', 'TARGET', 'DHI','DNI','WS', 'RH', 'T']]
-    temp = temp.assign(GHI=lambda x: x['DHI'] + x['DNI'] * np.cos(((180 * (x['Hour']+1) / 24) - 90)/180*np.pi))
-    temp = temp[['Hour','Minute', 'TARGET', 'DHI','DNI','WS', 'RH', 'T','GHI']]
-    if is_train==True:
-        temp['Target1'] = temp['TARGET'].shift(-48).fillna(method='ffill')
-        temp['Target2'] = temp['TARGET'].shift(-48*2).fillna(method='ffill')
-        return temp.iloc[:-96]
+import tensorflow.compat.v1 as tf
+from sklearn.preprocessing import MinMaxScaler
+tf.disable_v2_behavior()
 
-    elif is_train==False:  
-        return temp.iloc[-48:, :]
+x1_data = []#time
+x2_data = []
+x3_data = []
+x4_data = []
+x5_data = []
+x6_data = []#tempertaure
+y_data = []#target
 
-df_train = preprocess_data(train)
 
-test = []
+input_file = './data/train/train.csv'
 
-for i in range(81):
-    file_path = './data/test/' + str(i) + '.csv'
-    temp = pd.read_csv(file_path)
-    temp = preprocess_data(temp, is_train=False).iloc[-48:]
-    test.append(temp)
+with open(input_file) as file:
+  file.readline()
+  for line in file:
+    temp1 = 0
+    temp1_1 = float(line.split(',')[1])#hour
+    temp1_2 = float(line.split(',')[2])#minute
+    temp2 = float(line.split(',')[3])#
+    temp3 = float(line.split(',')[4])#
+    temp4 = float(line.split(',')[5])#
+    temp5 = float(line.split(',')[6])#
+    temp6 = float(line.split(',')[7])#temperature
+    tempY = float(line.split(',')[8])#target
 
-X_test = pd.concat(test)
+    if tempY == 0: continue
 
-x1_data = [73., 93., 89., 96., 73.]
-x2_data = [80., 88., 91., 98., 66.]
-x3_data = [75., 93., 90., 100., 70.]
-y_data = [72., 88., 92., 100., 71.]
- 
+    if temp1_2 == 0: 
+      temp1 = temp1_1
+    else : temp1 = temp1_1 + 0.5
+
+    x1_data.append(temp1)
+    x2_data.append(temp2)
+    x3_data.append(temp3)
+    x4_data.append(temp4)
+    x5_data.append(temp5)
+    x6_data.append(temp6)
+    y_data.append(tempY)
+
+
+scaler = MinMaxScaler()
+scaler.fit(x1_data)
+scaler.fit(x2_data)
+
+print(x1_data)
+print(x2_data)
+
+
 # 변수
 x1 = tf.placeholder(tf.float32)
 x2 = tf.placeholder(tf.float32)
 x3 = tf.placeholder(tf.float32)
+x4 = tf.placeholder(tf.float32)
+x5 = tf.placeholder(tf.float32)
+x6 = tf.placeholder(tf.float32)
 Y = tf.placeholder(tf.float32)
 
 w1 = tf.Variable(tf.random_normal([1]), name='weight1')
 w2 = tf.Variable(tf.random_normal([1]), name='weight2')
 w3 = tf.Variable(tf.random_normal([1]), name='weight3')
+w4 = tf.Variable(tf.random_normal([1]), name='weight4')
+w5 = tf.Variable(tf.random_normal([1]), name='weight5')
+w6 = tf.Variable(tf.random_normal([1]), name='weight6')
 b = tf.Variable(tf.random_normal([1]), name='bias')
-
+ 
 # 가설
-hypothesis = x1*w1 + x2*w2 + x3*w3 + b
-print (w1) 
+hypothesis = x1*w1 + x2*w2 + x3*w3 + x4*w4 + x5*w5 + x6*w6 + b
+ 
 # cost/loss function
 cost = tf.reduce_mean(tf.square(hypothesis - Y))
-
-
 # cost function 최소화
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-5)
-
-
 train = optimizer.minimize(cost)
- 
-# 세션 생성
-with tf.Session() as sess:
-    # 사용할 변수 선언
-    sess.run(tf.global_variables_initializer())
- 
-    for step in range(100001):
-        cost_val, hy_val, _,w1_val,w2_val,w3_val,b_val, = sess.run([cost, hypothesis, train,w1,w2,w3,b],
-                                       feed_dict={x1: x1_data, x2: x2_data, x3: x3_data, Y: y_data})
-        if step % 10 == 0:
-            print(step, "Cost: ", cost_val, "\nPrediction:\n", hy_val)
-print(w1_val)
-print(w2_val)
-print(w3_val)
-print(b_val)
+
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+
+for step in range(1001):
+  cost_val, hy_val,_  = sess.run([cost, hypothesis, train], feed_dict={x1: x1_data, x2: x2_data, x3: x3_data, x4: x4_data, x5: x5_data, x6: x6_data, Y: y_data})
+  if step % 50 == 0:
+    print(step, "\nCost: ", cost_val, "\nPrediction:\n", hy_val)
+  #if cost_val < 7 : break
+
+
+#print(w1_value)
+#print(w2_value)
+#print(w3_value)
+#print(b_value)
+print(sess.run(w1),sess.run(w2),sess.run(w3),sess.run(w4),sess.run(w5),sess.run(w6),sess.run(b))
